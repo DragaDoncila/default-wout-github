4a6f6b26d36871bcaf97a5a2267df2869cfe3459
Draga Doncila Pop <ddoncila@gmail.com>
2022-01-21 07:35:58 +1100

    initial commit

---
+++ src/default_wout_github/_tests/test_sample_data.py
@@ -0,0 +1,7 @@
+# from default_wout_github import make_sample_data
+
+# add your tests here...
+
+
+def test_something():
+    pass
---
+++ src/default_wout_github/_tests/test_writer.py
@@ -0,0 +1,7 @@
+# from default_wout_github import write_single_image, write_multiple
+
+# add your tests here...
+
+
+def test_something():
+    pass
